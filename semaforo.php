0dbc8a21acc004708dbf4ed778d48160bf18d487
AlloJS <eventorganization1@gmail.com>
2022-07-16 19:26:43 +0200

    Relase semaforo😀

---
+++ semaforo.php
@@ -0,0 +1,25 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="semaforo.css">
+  </head>
+  <body>
+    <div>
+      <h1>
+       Semaforo
+      </h1>
+    </div>
+    <main>
+      <div id="red" class="red">
+      </div>
+      <div id="yellow" class="yellow">
+      </div>
+      <div id="green" class="green">
+      </div>
+    </main>
+    <script src="semaforo.js" charset="utf-8"></script>
+  </body>
+</html>
